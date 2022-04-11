67e0faddf5576a3134297fab8cabe51628227b6c
Matias <matias_arenhard@hotmail.com>
2022-04-10 23:17:12 -0300

    create function to list users github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [username, setUsername] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    await fetch(`https://api.github.com/search/users?q=${query}`)
+      .then(response => response.json())
+      .then(data => {
+        setSearchResults(data.items);
+      });
+    setIsLoading(false);
+  }
+
+  return( <main>
+    <section className='search'>
+      <input type="search"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+        placeholder='Username' />
+      
+      <button onClick={handleSearch} disabled={ isLoading }>
+        { isLoading ? "Searching..." : "Search"}
+      </button>
+
+      {!!searchResults.length && (<>
+        <h1>Results :</h1>
+        <ul>
+          {searchResults.map((user) => (
+            <li key={user.id}>
+              <img src={user.avatar_url} alt={`Photo of ${user.login}`} />
+              {user.login}
+              {username === user.login ? " ✓" : (
+                <button onClick={() => setUsername(user.login)}>Select</button>  
+              )}
+            </li>
+          ))} 
+        </ul>
+      </>)}
+    </section>
+    <section className='repos'>Repositories</section>
+  </main >);
 }
 
 export default App;
